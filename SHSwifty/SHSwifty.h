80e5c9d365aad9709da6ab1db9d4e49423794fee
EthanXingyuJi <endorathas@gmail.com>
2018-01-07 23:47:34 +1100

    Initial Commit

---
+++ SHSwifty/SHSwifty.h
@@ -0,0 +1,19 @@
+//
+//  SHSwifty.h
+//  SHSwifty
+//
+//  Created by Xingyuji on 7/1/18.
+//  Copyright © 2018 com.xingyuji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SHSwifty.
+FOUNDATION_EXPORT double SHSwiftyVersionNumber;
+
+//! Project version string for SHSwifty.
+FOUNDATION_EXPORT const unsigned char SHSwiftyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHSwifty/PublicHeader.h>
+
+
