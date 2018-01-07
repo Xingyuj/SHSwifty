99d4cc547c33226a79fdc1a5878d213edc347780
EthanXingyuJi <endorathas@gmail.com>
2018-01-08 00:09:09 +1100

    update

---
+++ SHSwifty/Demo.swift
@@ -0,0 +1,17 @@
+//
+//  Demo.swift
+//  SHSwifty
+//
+//  Created by Xingyuji on 7/1/18.
+//  Copyright © 2018 com.xingyuji. All rights reserved.
+//
+
+import Foundation
+
+@objc public class SHApiProcessor:NSObject {
+    
+    @objc public static func printHello(){
+        print("hello fastlane and cocoapods")
+    }
+    
+}
